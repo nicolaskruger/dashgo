6db5837625b8ad024233da53e408f0ec4f0538f7
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-29 16:25:24 -0300

    feat:dash board

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,65 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import { NextPage } from "next";
+import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+
+const Chart = dynamic(() => import("react-apexcharts"), {
+    ssr: false
+})
+
+const options = {
+    chart: {
+        toolbar: {
+            show: false,
+        },
+        zoom: {
+            enable: false
+        },
+        foreColor: theme.colors.gray[500]
+    },
+    grid: {
+        show: false
+    },
+    dataLabels: {
+        enabled: false
+    },
+    tooltip: {
+        enabled: false
+    },
+    xaxis: {
+        type: "datetime",
+        axisBorder: {
+            color: theme.colors.gray[600]
+        },
+        axisTicks: {
+            color: theme.colors.gray[600]
+        },
+        categories: [
+            '2021-12-29T00:00:00.000Z',
+            '2021-12-30T00:00:00.000Z',
+            '2021-12-31T00:00:00.000Z',
+            '2022-01-01T00:00:00.000Z',
+            '2022-01-02T00:00:00.000Z',
+            '2022-01-03T00:00:00.000Z',
+            '2022-01-04T00:00:00.000Z',
+        ]
+    },
+    fill: {
+        opacity: 0.3,
+        type: "gradient",
+        gradient: {
+            shade: "dark",
+            opacityFrom: 0.7,
+            opacityTo: 0.3
+        }
+    }
+};
+
+const series = [
+    { name: 'series1', data: [31, 120, 10, 28, 61, 18, 109] }
+]
+
 
 const DashBoard: NextPage = () => {
     return (
@@ -15,6 +73,40 @@ const DashBoard: NextPage = () => {
                 px={"6"}
             >
                 <Sidebar />
+                <SimpleGrid
+                    flex={"1"}
+                    gap={"4"}
+                    minChildWidth={"320px"}
+                >
+                    <Box
+                        p="8"
+                        bg={"gray.800"}
+                        borderRadius={8}
+                        pb={"4"}
+                    >
+                        <Text
+                            fontSize={"lg"}
+                            mb={"4"}
+                        >
+                            Inscritos da semana
+                        </Text>
+                        <Chart options={options} series={series} type="area" height={160} />
+                    </Box>
+                    <Box
+                        p="8"
+                        bg={"gray.800"}
+                        borderRadius={8}
+                        pb={"4"}
+                    >
+                        <Text
+                            fontSize={"lg"}
+                            mb={"4"}
+                        >
+                            Taxa de abertura
+                        </Text>
+                        <Chart options={options} series={series} type="area" height={160} />
+                    </Box>
+                </SimpleGrid>
             </Flex>
         </Flex>
     )
