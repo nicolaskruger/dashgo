56da474b2b6288841cd429e86cc0e774cbc6b350
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-04 15:56:13 -0300

    configurnado react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,22 @@ import { SideBarDrawerProvider } from '../contexts/SidebarDrawerContext'
 import { makeServer } from '../services/mirage'
 import { theme } from '../styles/theme'
 
+import { QueryClient, QueryClientProvider } from "react-query";
 if (process.env.NODE_ENV === "development") {
   makeServer()
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SideBarDrawerProvider>
-        <Component {...pageProps} />
-      </SideBarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SideBarDrawerProvider>
+          <Component {...pageProps} />
+        </SideBarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,28 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Layout } from "../../components/Layot";
 import { Pagination } from "../../components/Pagination";
+import { dateFormat } from "../../services/dateFromat";
+import { useQuery } from "react-query";
+
+type User = {
+    name: string,
+    email: string,
+    createdAt: string;
+}
 
 const UserList: NextPage = () => {
+
+    const { data, isLoading, error } = useQuery("users", async () => {
+        const response = await fetch('http://localhost:3000/api/users');
+        const data = await response.json();
+        return data
+    })
+
+    const [users, setUsers] = useState<User[]>([])
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -14,9 +30,6 @@ const UserList: NextPage = () => {
     })
 
     useEffect(() => {
-        fetch('http://localhost:3000/api/users')
-            .then(response => response.json())
-            .then(data => console.log(data))
     }, [])
 
     return (
@@ -39,76 +52,99 @@ const UserList: NextPage = () => {
                             </Button>
                         </Link>
                     </Flex>
-                    <Table
-                        colorScheme={"whiteAlpha"}
-                    >
-                        <Thead>
-                            <Tr>
-                                <Th
-                                    px={["4", "4", "6"]}
-                                    color={"green.300"}
-                                    width={"8"}
+                    {
+                        isLoading ? (
+                            <Flex
+                                justify={"center"}
+                            >
+                                <Spinner />
+                            </Flex>
+                        ) : error ? (
+                            <Flex
+                                justify={"center"}
+                            >
+                                <Text>
+                                    Falha ao obter os dados
+                                </Text>
+                            </Flex>
+                        ) : (
+                            <>
+                                <Table
+                                    colorScheme={"whiteAlpha"}
                                 >
-                                    <Checkbox colorScheme={"pink"} />
-                                </Th>
-                                <Th>
-                                    Usuário
-                                </Th>
-                                {isWideVersion &&
-                                    <Th>
-                                        Data de cadastro
-                                    </Th>
-                                }
-                                <Th>
+                                    <Thead>
+                                        <Tr>
+                                            <Th
+                                                px={["4", "4", "6"]}
+                                                color={"green.300"}
+                                                width={"8"}
+                                            >
+                                                <Checkbox colorScheme={"pink"} />
+                                            </Th>
+                                            <Th>
+                                                Usuário
+                                            </Th>
+                                            {isWideVersion &&
+                                                <Th>
+                                                    Data de cadastro
+                                                </Th>
+                                            }
+                                            <Th>
 
-                                </Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td
-                                    px={["4", "4", "6"]}
-                                    color={"green.300"}
-                                    width={"8"}
-                                >
-                                    <Checkbox colorScheme={"pink"} />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight={"bold"}>
-                                            Nícolas Krüger
-                                        </Text>
-                                        <Text fontSize={"sm"} color={"gray.300"}>
-                                            nicolas.nkruger@gmail.com
-                                        </Text>
-                                    </Box>
-                                </Td>
-                                {
-                                    isWideVersion &&
-                                    <Td>
-                                        04 de Abril, 2021
-                                    </Td>
-                                }
-                                <Td>
-                                    {isWideVersion &&
-                                        <Button
-                                            as="a"
-                                            size={"sm"}
-                                            fontSize={"sm"}
-                                            colorScheme={"purple"}
-                                            leftIcon={<Icon as={RiPencilLine}
-                                                fontSize={"16"} />}
-                                        >
-                                            Editar
-                                        </Button>
-                                    }
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
-                </Box>
-            </Layout>
+                                            </Th>
+                                        </Tr>
+                                    </Thead>
+                                    <Tbody>
+                                        {users.map((user, index) => (
+
+                                            <Tr key={index}>
+                                                <Td
+                                                    px={["4", "4", "6"]}
+                                                    color={"green.300"}
+                                                    width={"8"}
+                                                >
+                                                    <Checkbox colorScheme={"pink"} />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight={"bold"}>
+                                                            {user.name}
+                                                        </Text>
+                                                        <Text fontSize={"sm"} color={"gray.300"}>
+                                                            {user.email}
+                                                        </Text>
+                                                    </Box>
+                                                </Td>
+                                                {
+                                                    isWideVersion &&
+                                                    <Td>
+                                                        {dateFormat(user.createdAt)}
+                                                    </Td>
+                                                }
+                                                <Td>
+                                                    {isWideVersion &&
+                                                        <Button
+                                                            as="a"
+                                                            size={"sm"}
+                                                            fontSize={"sm"}
+                                                            colorScheme={"purple"}
+                                                            leftIcon={<Icon as={RiPencilLine}
+                                                                fontSize={"16"} />}
+                                                        >
+                                                            Editar
+                                                        </Button>
+                                                    }
+                                                </Td>
+                                            </Tr>
+                                        ))}
+                                    </Tbody>
+                                </Table >
+                                <Pagination />
+                            </>
+                        )
+                    }
+                </Box >
+            </Layout >
         </Box >
     )
 }
